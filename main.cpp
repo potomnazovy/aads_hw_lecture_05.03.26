edb7e78c39c5961de4620f35925ded7c30097536
potomnazovy <skeleton39876@mail.ru>
2026-03-03 18:57:52 +0300

    add task

--- main.cpp
+++ main.cpp
@@ -1,6 +1,201 @@
 #include <iostream>
+#include <cstddef>
+#include <stdexcept>
+
+template< class T >
+struct BiList
+{
+  T val;
+  BiList< T >* next;
+  BiList< T >* prev;
+};
+
+template< class T >
+BiList< T >* create(const T& value)
+{
+  BiList< T >* node = new BiList< T >{value, nullptr, nullptr};
+  node->next = node;
+  node->prev = node;
+  return node;
+}
+
+template< class T >
+BiList< T >* add(BiList< T >* h, const T& value)
+{
+  BiList< T >* newNode = new BiList< T >{value, h, h->prev};
+  h->prev->next = newNode;
+  h->prev = newNode;
+  return newNode;
+}
+
+template< class T >
+BiList< T >* insert(BiList< T >* h, const T& value)
+{
+  return add(h->next, value);
+}
+
+template< class T >
+BiList< T >* cut(BiList< T >* h) noexcept
+{
+  BiList< T >* ret = h->next;
+  h->prev->next = ret;
+  ret->prev = h->prev;
+  delete h;
+  return ret;
+}
+
+template< class T >
+BiList< T >* erase(BiList< T >* h) noexcept
+{
+  return cut(h->next);
+}
+
+template < class T >
+BiList< T >* clear(BiList< T >* fake) noexcept
+{
+  while (fake->next != fake)
+  {
+    erase(fake);
+  }
+  return fake;
+}
+
+template<class T>
+BiList< T >* fake(BiList< T >* h)
+{
+  BiList< T >* last = h->prev;
+  BiList< T >* f = static_cast< BiList< T >* >(::operator new(sizeof(BiList< T >)));
+  f->next = h;
+  h->prev = f;
+  last->next = f;
+  f->prev = last;
+  return f;
+}
+
+template< class T >
+BiList< T >* rmfake(BiList< T >* h)
+{
+  BiList< T >* first = h->next;
+  BiList< T >* last = h->prev;
+  last->next = first;
+  first->prev = last;
+  ::operator delete(h);
+  return first;
+}
+
+template< class T >
+BiList< T >* convert(const T* a, size_t size)
+{
+  if (size == 0)
+  {
+    return nullptr;
+  }
+  BiList< T >* head = nullptr;
+  BiList< T >* curr = nullptr;
+  try
+  {
+    head = create(a[0]);
+    curr = head;
+    for (size_t i = 1; i < size; ++i)
+    {
+      curr = insert(curr, a[i]);
+    }
+    curr->next = head;
+    head->prev = curr;
+  }
+  catch (...)
+  {
+    if (head)
+    {
+      BiList< T >* tempFake = fake(head);
+      clear(tempFake);
+      delete tempFake;
+    }
+    throw;
+  }
+  return fake(head);
+}
+
+template< class T, class F >
+F traverse(F f, BiList< T >* h, BiList< T >* e)
+{
+  BiList< T >* curr = h;
+  while (curr != e)
+  {
+    f(curr->val);
+    curr = curr->next;
+  }
+  return f;
+}
+
+template< class T, class F >
+F traverse_back(F f, BiList< T >* h, BiList< T >* e)
+{
+  BiList< T >* curr = h;
+  while (curr != e)
+  {
+    f(curr->val);
+    curr = curr->prev;
+  }
+  return f;
+}
 
 int main()
 {
+  size_t size = 5;
+  int* arr = new int[size]{10, 20, 30, 40, 50};
+
+  BiList< int >* list = nullptr;
+  try
+  {
+    list = convert(arr, size);
+
+    auto print = [](int x) { std::cout << x << " "; };
+
+    std::cout << "Прямой порядок: ";
+    traverse(print, list->next, list);
+    std::cout << '\n';
+
+    std::cout << "Вставка 99 после второго элемента:" << '\n';
+    insert(list->next->next, 99);
+
+    std::cout << "После вставки: ";
+    traverse(print, list->next, list);
+    std::cout << '\n';
+
+    std::cout << "Обратный порядок: ";
+    traverse_back(print, list->prev, list);
+    std::cout << '\n';
+
+    std::cout << "Вставка 99 перед вторым элементом:" << '\n';
+    add(list->next->next, 99);
+
+    std::cout << "После вставки: ";
+    traverse(print, list->next, list);
+    std::cout << '\n';
+
+    std::cout << "Удаление третьего элемента:" << '\n';
+    erase(list->next->next);
+
+    std::cout << "После удаления: ";
+    traverse(print, list->next, list);
+    std::cout << '\n';
+
+    clear(list);
+    delete list;
+    list = nullptr;
+
+    std::cout << "Список очищен." << '\n';
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << "Error: " << e.what() << std::endl;
+  }
+  catch (...)
+  {
+    std::cerr << "Unknown error" << '\n';
+  }
+
+  delete[] arr;
   return 0;
 }
